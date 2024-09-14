be2a7293cff023316ff06d50ffdf2b8f003a0eda
Mahe-22 <santhoshmuthu93611@gmail.com>
2024-09-14 20:52:32 +0530

    Edit_Req_Histories_Table_Added.

---
+++ Core/Migrations/20240914044905_EditReqHistories_table_added.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NOCIL_VP.Domain.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditReqHistories_table_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EditRequestHistories",
+                columns: table => new
+                {
+                    Log_Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Form_Id = table.Column<int>(type: "int", nullable: false),
+                    VendorCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RequestedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsApproved = table.Column<bool>(type: "bit", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EditRequestHistories", x => x.Log_Id);
+                    table.ForeignKey(
+                        name: "FK_EditRequestHistories_Forms_Form_Id",
+                        column: x => x.Form_Id,
+                        principalTable: "Forms",
+                        principalColumn: "Form_Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EditRequestHistories_Form_Id",
+                table: "EditRequestHistories",
+                column: "Form_Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EditRequestHistories");
+        }
+    }
+}
