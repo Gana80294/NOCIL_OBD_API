d1d4f37c5691b824b59d7abaeb4f5b63f4aeaa2e
Mahe-22 <santhoshmuthu93611@gmail.com>
2024-09-17 10:12:00 +0530

    Modified in EditRequest_HistoryTable

---
+++ Core/Migrations/20240916102249_RejectedBy_column_added_in_EditReqhistory_table.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NOCIL_VP.Domain.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RejectedBy_column_added_in_EditReqhistory_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rejectedby",
+                table: "EditRequestHistories",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rejectedby",
+                table: "EditRequestHistories");
+        }
+    }
+}
---
+++ Core/Migrations/20240916102445_RejectedBy_column_modified_in_EditReqhistory_table.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NOCIL_VP.Domain.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RejectedBy_column_modified_in_EditReqhistory_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
